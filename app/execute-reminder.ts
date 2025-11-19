d1244c4acd6c71736ab1db6879d13ce64429d1f9
Josiah Sayers <josiah.sayers15@gmail.com>
2025-11-19 00:00:12 -0500

    setup network printer for reminders

--- app/execute-reminder.ts
+++ app/execute-reminder.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../prisma/db";
 import type { Reminder } from "../prisma/generated/client";
 import { logger } from "./utils/logger";
+import { printReminder } from "./utils/printer";
 
 export async function executeReminder(reminder: Reminder) {
   let successful = true;
@@ -8,6 +9,7 @@ export async function executeReminder(reminder: Reminder) {
   try {
     // todo: replace with actual implementation
     logger.verbose("Sending reminder", { reminder });
+    await printReminder(reminder);
   } catch (e) {
     successful = false;
     logger.error("Error sending reminder", e, { reminder });
---
+++ app/utils/printer.ts
@@ -0,0 +1,62 @@
+import {
+  ThermalPrinter,
+  PrinterTypes,
+  CharacterSet,
+} from "node-thermal-printer";
+import { logger } from "./logger";
+import type { Reminder } from "../../prisma/generated/client";
+
+export async function getPrinter() {
+  const printer = new ThermalPrinter({
+    type: PrinterTypes.EPSON,
+    interface: `tcp://${Bun.env.PRINTER_IP}`,
+    characterSet: CharacterSet.PC852_LATIN2,
+    width: 42,
+  });
+
+  const isConnected = await printer.isPrinterConnected();
+  if (!isConnected) {
+    logger.error("Unable to connect to printer");
+  }
+
+  return printer;
+}
+
+export async function printReminder(reminder: Reminder) {
+  const printer = await getPrinter();
+
+  // Title
+  printer.alignCenter();
+  printer.underline(true);
+  printer.bold(true);
+  printer.setTextDoubleHeight();
+  printer.println(reminder.title);
+  printer.newLine();
+
+  // Content
+  printer.underline(false);
+  printer.bold(false);
+  printer.setTextNormal();
+  printer.alignLeft();
+  // Sequential newline characters get truncated to a single newline. This forces them to print correctly.
+  reminder.content.split("\n").forEach((segment) => {
+    if (segment) {
+      printer.println(segment);
+    } else {
+      printer.newLine();
+    }
+  });
+
+  // Logo
+  printer.drawLine();
+  printer.alignCenter();
+  await printer.printImage("./app/assets/logo.png");
+
+  printer.cut();
+
+  try {
+    await printer.execute();
+  } catch (e) {
+    logger.error("Failed to print reminder", e, { reminder });
+  }
+}
